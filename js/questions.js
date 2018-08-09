69882e594ce0e354e5dfa4aa15792a79a5bf13f3
Fernando Danko <fernandodanko@gmail.com>
2018-08-09 11:19:34 -0300

    Reducida la cantidad de preguntas

--- js/questions.js
+++ js/questions.js
@@ -7,18 +7,14 @@ Questions = {
     {id: 0, question: "Asiste normalmente a clase", short: "Asistencia a clase"},
     {id: 1, question: "Cumple con los horarios establecidos", short: "Cumple los horarios"},
     {id: 2, question: "Mantiene un trato adecuado con los alumnos", short: "Mantiene un trato adecuado"},
-    {id: 3, question: "Parece dominar la asignatura que imparte", short: "Sabe de la materia"},
-    {id: 4, question: "Sus clases están bien organizadas"},
-    {id: 5, question: "Explica con claridad"},
-    {id: 6, question: "Varía las estrategias de enseñanza para asegurar la comprensión, aclarar dudas o atender necesidades individuales", short: "Varia la forma de enseñar según se necesite"},
-    {id: 7, question: "Intenta que los alumnos participen en las clases", short: "Fomenta la participación en clase"},
-    {id: 8, question: "Presenta un panorama amplio de su asignatura"},
-    {id: 9, question: "Responde con exactitud y precisión a las preguntas que le hacen"},
-    {id: 10, question: "Acepta la crítica fundamentada"},
-    {id: 11, question: "Utiliza en clase un material didáctico que ayuda a comprender las explicaciones"},
-    {id: 12, question: "Responde dudas por mail/campus"}
+    {id: 3, question: "Sus clases están bien organizadas"},
+    {id: 4, question: "Explica con claridad"},
+    {id: 5, question: "Intenta que los alumnos participen en las clases", short: "Fomenta la participación en clase"},
+    {id: 6, question: "Presenta un panorama amplio de su asignatura"},
+    {id: 7, question: "Acepta la crítica fundamentada"},
+    {id: 8, question: "Responde dudas por mail/campus"}
   ],
-  activeQuestions: [0,1,2,3,4,5,6,7,8,9,10,11,12],
+  activeQuestions: [0,1,2,3,4,5,6,7,8],
   getQuestions: function(){
     return Questions.questions.filter(q => Questions.activeQuestions.includes(q.id));
   }
