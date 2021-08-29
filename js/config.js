d2ac9cf3de5413063e60ceaecf5bfec40f1388d6
Lucas Sotelo <lucasnsotelog@gmail.com>
2021-08-29 00:55:06 -0300

    Badges para indicar cuatrimeste y virtualidad

--- js/config.js
+++ js/config.js
@@ -5,19 +5,19 @@ Config = {
   cursosPath: "./analitics/cursos/",
   equivalencyData: "equivalencias.json",
   json_list: [  // File names: Horarios_REF.json
-    {name: "1º Cuat 2015", ref: "1Q2015"},
-    {name: "2º Cuat 2015", ref: "2Q2015"},
-    {name: "1º Cuat 2016", ref: "1Q2016"},
-    {name: "2º Cuat 2016", ref: "2Q2016"},
-    {name: "1º Cuat 2017", ref: "1Q2017"},
-    {name: "2º Cuat 2017", ref: "2Q2017"},
-    {name: "1º Cuat 2018", ref: "1Q2018"},
-    {name: "2º Cuat 2018", ref: "2Q2018"},
-    {name: "1º Cuat 2019", ref: "1Q2019"},
-    {name: "2º Cuat 2019", ref: "2Q2019"},
-    {name: "1º Cuat 2020", ref: "1Q2020"},
-    {name: "2º Cuat 2020", ref: "2Q2020"},
-	{name: "1º Cuat 2021", ref: "1Q2021"}
+    {name: "1º Cuat 2015", ref: "1Q2015", virtual: false},
+    {name: "2º Cuat 2015", ref: "2Q2015", virtual: false},
+    {name: "1º Cuat 2016", ref: "1Q2016", virtual: false},
+    {name: "2º Cuat 2016", ref: "2Q2016", virtual: false},
+    {name: "1º Cuat 2017", ref: "1Q2017", virtual: false},
+    {name: "2º Cuat 2017", ref: "2Q2017", virtual: false},
+    {name: "1º Cuat 2018", ref: "1Q2018", virtual: false},
+    {name: "2º Cuat 2018", ref: "2Q2018", virtual: false},
+    {name: "1º Cuat 2019", ref: "1Q2019", virtual: false},
+    {name: "2º Cuat 2019", ref: "2Q2019", virtual: false},
+    {name: "1º Cuat 2020", ref: "1Q2020", virtual: true},
+    {name: "2º Cuat 2020", ref: "2Q2020", virtual: true},
+		{name: "1º Cuat 2021", ref: "1Q2021", virtual: true}
   ],
   CSVDelim: ","
 }
