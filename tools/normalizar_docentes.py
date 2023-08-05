bab1ddb609ce3b83d8130c49e79127d2bf9e66b1
walgab <gabriel.diem5@gmail.com>
2023-08-05 18:46:45 -0300

    Actualizo script de python para nuevo formato JSON

--- tools/normalizar_docentes.py
+++ tools/normalizar_docentes.py
@@ -7,14 +7,23 @@ y transforma los docentes de la siguiente forma:
 APELLIDO, NOMBRE NOMBRE - APELLIDO, NOMBRE NOMBRE -> Apellido-Apellido
 """
 
-DATA_DIR = Path(Path.cwd() / "data")
-HORARIOS_FILENAME = "Horarios_1Q2020.json" # Cambiar el archivo a usar
+DATA_DIR = Path(Path.cwd())
+HORARIOS_FILENAME = "horarios.json" # Cambiar el archivo a usar
 
-with open(DATA_DIR / HORARIOS_FILENAME, 'r+') as f:
+def obtenerCurso(codigoCurso, cursos):
+    for curso in cursos:
+        if(curso["codigo"] == codigoCurso):
+            print(curso)
+            return curso
+
+
+with open(DATA_DIR / HORARIOS_FILENAME, 'r+', encoding='utf-8') as f:
     data = json.load(f)
     f.seek(0)
+    cursos = data["cursos"]
     for materia in data['materias']:
-        for curso in materia['cursos']:
+        for codigoCurso in materia["cursos"]:
+            curso = obtenerCurso(codigoCurso, cursos)
             docentes = [d.strip() for d in curso['docentes'].split('-')]
             docentes_apellidos = [d.split(',')[0].title() for d in docentes] # Me quedo solo con el apellido
             docentes_final = '-'.join(docentes_apellidos)
