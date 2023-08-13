c68e339829f641007a804fb9920357e579e857e4
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2023-08-12 21:15:10 -0300

    test(cuatrimestre): should return equivalent materia codes

--- src/analytics/analysis-by-cuatri.spec.ts
+++ src/analytics/analysis-by-cuatri.spec.ts
@@ -1,5 +1,5 @@
 import {describe, expect} from "@jest/globals";
-import {analizar_equivalencias, emprolijar_docentes} from "./analysis-by-cuatri";
+import {analizar_equivalencias, buscar_codigos_materias_equivalentes, emprolijar_docentes} from "./analysis-by-cuatri";
 
 describe('emprolijar_docentes', () => {
 	it('should remove "A Designar" from docentes_raw list', () => {
@@ -34,5 +34,24 @@ describe('analizar_equivalencias', () => {
 		const result = analizar_equivalencias(input);
 
 		expect(result).toEqual({'7533': ['7533', '7543', '9560'], '7543': ['7533', '7543', '9560'], '9560': ['7533', '7543', '9560'], '7541': ['7541', '9515'], '9515': ['7541', '9515'], '7504': ['7504', '9512'], '9512': ['7504', '9512'], '7515': ['7515', '7528', '9505'], '7528': ['7515', '7528', '9505'], '9505': ['7515', '7528', '9505']});
+	});
+});
+
+describe("buscar_codigos_materias_equivalentes", () => {
+	const equivalencias = analizar_equivalencias([[7533, 7543, 9560],
+		[7541, 9515],
+		[7504, 9512],
+		[7515, 7528, 9505]]);
+
+	it("should return equivalent materia codes", () => {
+		const materia = "7504";
+		const equivalentCodes = buscar_codigos_materias_equivalentes(materia, equivalencias);
+		expect(equivalentCodes).toEqual(["9512"]);
+	});
+
+	it("should handle non-existent materia", () => {
+		const materia = "9999"; // Non-existent
+		const equivalentCodes = buscar_codigos_materias_equivalentes(materia, equivalencias);
+		expect(equivalentCodes).toEqual([]);
 	});
 });
--- src/analytics/analysis-by-cuatri.ts
+++ src/analytics/analysis-by-cuatri.ts
@@ -1,3 +1,5 @@
+import {Materia} from "~/analytics/curso";
+
 export function emprolijar_docentes(docentes_raw: string): string[] {
 	const docentes_raw_list = docentes_raw.split('-');
 	if (docentes_raw_list.includes("A Designar")) {
@@ -16,3 +18,17 @@ export function analizar_equivalencias(eq: number[][]): { [key: string]: string[
 	return equivalencias;
 }
 
+export function buscar_codigos_materias_equivalentes(
+	materia: string,
+	equivalencias: { [key: string]: string[] }
+): string[] {
+	const equivalentMateriaCodigos: string[] = [];
+
+	for (const m of equivalencias[materia] || []) {
+		if (m !== materia) {
+			equivalentMateriaCodigos.push(m);
+		}
+	}
+
+	return equivalentMateriaCodigos;
+}
