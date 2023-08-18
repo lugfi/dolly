f5790945b99713a2c7fc38484baa59a9556b35e0
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2023-08-18 13:00:30 -0300

    feat: analizar cuatri

--- src/analytics/analysis-by-cuatri.ts
+++ src/analytics/analysis-by-cuatri.ts
@@ -1,6 +1,7 @@
-import {Docente, Materia} from "./curso";
+import {Curso, Docente, Materia} from "./curso";
 import {readCSVFile} from "./analysis";
 import {RowData} from "./types";
+import * as fs from "fs";
 
 export function emprolijar_docentes(docentes_raw: string): string[] {
 	const docentes_raw_list = docentes_raw.split('-');
@@ -119,6 +120,61 @@ export function buscar_docente(
 	dict_docentes[docente] = newDocente;
 	return newDocente;
 }
+
+export function analizarCuatri(archivo: string): void {
+	const eq = JSON.parse(fs.readFileSync('../data/equivalencias.json', 'utf8'));
+	const equivalencias = analizar_equivalencias(eq);
+	console.log('Analizando cuatri...');
+
+	//const cuatriParts = archivo.split('_');
+	//const cuatri = cuatriParts[cuatriParts.length - 1].split('.')[0];
+
+	const rawData = fs.readFileSync(archivo, 'utf8');
+	const js = JSON.parse(rawData);
+	let id_actual = 0;
+	const materias: { [key: string]: Materia } = {};
+	const docentes: { [key: string]: Docente } = {};
+	const cursos: { [key: number]: Curso } = {};
+
+	for (const mat_id of js['materias']) {
+		const mat = new Materia(mat_id.codigo, mat_id.nombre);
+		materias[mat.codigo] = mat;
+	}
+
+	for (const mat_codigo of Object.keys(materias)) {
+		const materias_eq = buscar_codigos_materias_equivalentes(mat_codigo, equivalencias);
+		if (!materias_eq) {
+			continue;
+		}
+		for (const m of materias_eq) {
+			if (!(m in materias)) {
+				materias[m] = new Materia(m, materias[mat_codigo].nombre);
+			}
+		}
+	}
+
+	for (const mat_id of js['materias']) {
+		for (const curso_id of mat_id.cursos) {
+			const curso = new Curso(id_actual);
+			cursos[id_actual] = curso;
+			let docentes_raw = curso_id.docentes;
+			docentes_raw = emprolijar_docentes(docentes_raw);
+			const materias_eq = buscar_materias_equivalentes(mat_id.codigo, materias, equivalencias);
+			for (const d of docentes_raw) {
+				const doc = buscar_docente(d, materias_eq || [], docentes);
+				curso.agregar_docente(doc);
+			}
+			if (materias_eq !== null) {
+				for (const m of materias_eq) {
+					m.agregar_curso(curso);
+				}
+			}
+			id_actual++;
+		}
+		id_actual = 0;
+	}
+}
+
 
 
 
